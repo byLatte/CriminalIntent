45fd6731d4768ae31c35e8a1fba86732a539a1d3
dev21 <dev21>
2021-06-13 07:45:02 +0900

    framelayout으로 변경 및 fragment 적용

--- app/src/main/java/com/latte/crime/CrimeFragment.kt
+++ app/src/main/java/com/latte/crime/CrimeFragment.kt
@@ -1,14 +1,73 @@
 package com.latte.crime
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 
 class CrimeFragment: Fragment(){
 
     private lateinit var crime: Crime
+    private lateinit var titleField: EditText
+    private lateinit var dateButton: Button
+    private lateinit var solvedCheckBox: CheckBox
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         crime = Crime()
     }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_crime,container,false)
+
+        titleField = view.findViewById(R.id.crime_title) as EditText // EditText 로 변환
+        dateButton = view.findViewById(R.id.crime_date) as Button // EditText 로 변환
+        solvedCheckBox = view.findViewById(R.id.crime_solved) as CheckBox
+
+        dateButton.apply {
+            text = crime.date.toString()
+            isEnabled = false // 버튼 비활성화
+        }
+
+        return view
+    }
+
+    // textWatcher를 onStart에서 설정해줘야 뷰가 복원된 후 실행된다.
+    // 그전인 onCreate,onCreateView에서 설정하면 뷰가 복원되기전 실행이 된다.
+    override fun onStart() {
+        super.onStart()
+        val titleWatcher = object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTextChanged(sequence: CharSequence?, start: Int, before: Int, count: Int) {
+                crime.title = sequence.toString()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                TODO("Not yet implemented")
+            }
+        }
+
+        titleField.addTextChangedListener(titleWatcher)
+
+        solvedCheckBox.apply {
+            setOnCheckedChangeListener {_, isChecked -> // '_' parameter를 사용할 필요가 없어서 생략.
+                crime.isSolved = isChecked
+            }
+        }
+
+    }
+
 }
