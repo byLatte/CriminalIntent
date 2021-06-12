33013b59db7a7f5761e241ffaab08fb6b8e81423
dev21 <dev21>
2021-06-13 06:02:28 +0900

    init

---
+++ app/src/main/java/com/latte/crime/CrimeFragment.kt
@@ -0,0 +1,14 @@
+package com.latte.crime
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class CrimeFragment: Fragment(){
+
+    private lateinit var crime: Crime
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        crime = Crime()
+    }
+}
